501a2ae1687610f59ba138237747f7cb3f9e4b56
Rizal <mrizalpahlevi372@gmail.com>
2022-01-10 16:55:08 +0700

    Initial commit

---
+++ app/src/main/java/com/rizal/laundry/Constants.java
@@ -0,0 +1,12 @@
+package com.rizal.laundry;
+
+
+public class Constants {
+    public static final String URL= "http://192.168.1.13/";
+    public static final String ROOT_URL = URL+"crud_api/";
+    public static final String URL_ADD = ROOT_URL+"add.php";
+    public static final String URL_DELETE = ROOT_URL+"delete.php";
+    public static final String URL_UPDATE = ROOT_URL+"update.php";
+    public static final String URL_SELECT = ROOT_URL+"select.php";
+    public static final String URL_SELECT_ID = ROOT_URL+"select_id.php";
+}
